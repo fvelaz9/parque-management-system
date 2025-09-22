0e477a8647d9947306f13627ba393dbae0d495b5
Felipe Velazquez <feli.velazquez@gmail.com>
2025-09-22 20:30:49 -0300

    FUNCIONA

--- src/Parque.Dominio/Atracciones/AtraccionParque.cs
+++ src/Parque.Dominio/Atracciones/AtraccionParque.cs
@@ -5,7 +5,7 @@ public class AtraccionParque(string nombre, TipoAtraccion tipo, int edadMinima, 
     public int Id { get; set; }
     public string Nombre { get; set; } = nombre;
     public TipoAtraccion Tipo { get; set; } = tipo;
-    public int Edad_Minima { get; set; } = edadMinima;
+    public int EdadMinima { get; set; } = edadMinima;
     public int Capacidad { get; set; } = capacidadMaxima;
     public string Descripcion { get; set; } = descripcion;
     public EstadoAtraccion Estado { get; set; } = EstadoAtraccion.Disponible;
@@ -14,24 +14,4 @@ public class AtraccionParque(string nombre, TipoAtraccion tipo, int edadMinima, 
     {
         return Capacidad - aforoActual;
     }
-
-    /*public bool PuedeIngresar(Visitante visitante, DateTime fechaActual, int aforoActual)
-    {
-        if(Estado == EstadoAtraccion.FueraDeServicio)
-        {
-            return false;
-        }
-
-        if (visitante.Edad < EdadMinima)
-        {
-            return false;
-        }
-
-        if (aforoActual >= Capacidad)
-        {
-            return false;
-        }
-
-        return true;
-    }*/
 }
