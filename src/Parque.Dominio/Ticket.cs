0e2fd167a64ecd0079c5097c1f55bd3e3f2c9c20
Felipe Velazquez <feli.velazquez@gmail.com>
2025-09-22 19:07:33 -0300

    cambios ticket

--- src/Parque.Dominio/Ticket.cs
+++ src/Parque.Dominio/Ticket.cs
@@ -1,10 +1,11 @@
-namespace Parque.Dominio
+namespace Parque.Dominio;
+
 public class Ticket
 {
     public int Id { get; set; }
-    public int CuentaId  { get; set; }
-    public DateTime FechaVisita  { get; set; }
-    public int EventoId  { get; set; }
-    public Guid Codigo {get; set;}
-    public DateTime FechaEmision  { get; set; }
+    public int CuentaId { get; set; }
+    public DateTime FechaVisita { get; set; }
+    public int EventoId { get; set; }
+    public Guid Codigo { get; set; }
+    public DateTime FechaEmision { get; set; }
 }
