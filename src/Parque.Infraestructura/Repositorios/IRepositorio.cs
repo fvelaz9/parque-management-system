9d508d6bad08bf7ca78f648c1de33711526f998f
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-09-27 18:49:54 -0300

    Clase Irepositorio

---
+++ src/Parque.Infraestructura/Repositorios/IRepositorio.cs
@@ -0,0 +1,15 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parque.Infraestructura;
+
+public interface IRepositorio<T>
+{
+    void Agregar(T entity);
+    T? Encontrar(Expression<Func<T, bool>> predicate);
+    void Editar(T entity);
+    void Eliminar(Expression<Func<T, bool>> predicate);
+    IEnumerable<T> Obtener(Expression<Func<T, bool>> predicate);
+    IEnumerable<T> ObtenerLista();
+    
+}
