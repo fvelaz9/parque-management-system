cbc7abf3e838ab04d166ce77707dd9874cd7faaf
ponstomas <t1999pons@gmail.com>
2025-09-30 19:44:48 -0300

    feat: se crea carpeta para controllers webapi y healthcontroller

---
+++ src/Parque.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Parque.WebApi.Controllers;
+
+[ApiController]
+[Route("/", Name = "Ping")]
+[Route("health", Name = "Health Check")]
+public sealed class HealthController
+  : ControllerBase
+{
+    [HttpGet]
+    public object Get()
+    {
+        // Aquí puedes agregar chequeos personalizados (DB, servicios externos, etc.)
+        return new
+        {
+            v = "1.0",
+            alive = true
+        };
+    }
+}
