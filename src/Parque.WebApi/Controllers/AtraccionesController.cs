bbabe2a888d60e35080631487c6dba09e299a4cc
Felipe Velazquez <feli.velazquez@gmail.com>
2025-10-01 17:57:15 -0300

    Implementacion controller

---
+++ src/Parque.WebApi/Controllers/AtraccionesController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Parque.Aplicacion.Servicios.Atracciones;
+using Parque.Dominio.Atracciones;
+
+namespace Parque.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AtraccionesController : ControllerBase
+{
+    private readonly IServicioAtracciones _service;
+
+    public AtraccionesController(IServicioAtracciones service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll() => Ok(_service.ListarAtracciones());  
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var atraccion = _service.BuscarAtraccion(id);
+        return atraccion == null ? NotFound() : Ok(atraccion); 
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] AtraccionParque atraccion)
+    {
+        var creada = _service.CrearAtraccion(
+            atraccion.Nombre,
+            atraccion.Tipo,
+            atraccion.EdadMinima,
+            atraccion.Capacidad,
+            atraccion.Descripcion
+        );
+        return CreatedAtAction(nameof(GetById), new { id = creada.Id }, creada);  
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, [FromBody] AtraccionParque atraccion)
+    {
+        _service.ModificarAtraccion(
+            id,
+            atraccion.Nombre,
+            atraccion.Tipo,
+            atraccion.EdadMinima,
+            atraccion.Capacidad,
+            atraccion.Descripcion
+        );
+        return NoContent(); 
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        _service.EliminarAtraccion(id);
+        return NoContent();  
+    }
+}
