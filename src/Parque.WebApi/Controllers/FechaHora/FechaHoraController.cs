181a11a1647d1b3927fffd12c8c0941c8c8adc83
tomas pons <t1999pons@gmail.com>
2025-10-01 21:39:17 -0300

    feat: model para request al configurar fecha

---
+++ src/Parque.WebApi/Controllers/FechaHora/FechaHoraController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parque.Aplicacion.Servicios;
+using Parque.WebApi.Controllers.FechaHora.Models;
+
+namespace Parque.WebApi.Controllers.FechaHora;
+[Route("api/[controller]")]
+[ApiController]
+public class FechaHoraController(IServicioFechaHora timeService) : ControllerBase
+{
+    private readonly IServicioFechaHora _servicioFecha = timeService;
+
+    [HttpGet("actual")]
+    public IActionResult ObtenerFechaActual()
+    {
+        var currentTime = _servicioFecha.ObtenerFechaActual();
+        return Ok(new { datetime = currentTime });
+    }
+
+    [HttpPost("configurar")]
+    public IActionResult ConfigurarFecha([FromBody] ConfigurarFechaRequest request)
+    {
+        if (!DateTime.TryParse(request.FechaHora, out var fechaPersonalizada))
+        {
+            return BadRequest("Formato de fecha inválido. Use 'YYYY-MM-DDTHH:MM'.");
+        }
+
+        _servicioFecha.ConfigurarFecha(fechaPersonalizada);
+        return Ok(new { mensaje = "Fecha configurada exitosamente" });
+    }
+}
