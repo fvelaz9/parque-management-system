21dbcd758676aedfec1476fd2ced642e545d18c6
tomas pons <t1999pons@gmail.com>
2025-10-03 23:23:42 -0300

    feat: Interface servicio cuenta define contratos

--- src/Parque.Aplicacion/Servicios/IServicioCuenta.cs
+++ src/Parque.Aplicacion/Servicios/IServicioCuenta.cs
@@ -1,8 +1,13 @@
 ﻿using Parque.Aplicacion.DTOs.Usuarios;
+using Parque.Dominio.Usuarios;
 
 namespace Parque.Aplicacion.Servicios;
 public interface IServicioCuenta
 {
     CuentaDto RegistrarVisitante(RegistrarVisitanteDto dto);
+    CuentaDto CrearCuentaPorAdmin(RegistrarCuentaDto dto);
     void ModificarPerfil(Guid cuentaId, ModificarPerfilDto dto);
+    void CambiarNivelMembresia(Guid cuentaId, NivelMembresia nuevoNivel);
+    CuentaDto ObtenerPorId(Guid id);
+    CuentaDto ObtenerPorEmail(string email);
 }
