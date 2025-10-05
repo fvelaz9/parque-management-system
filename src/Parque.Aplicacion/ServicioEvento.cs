cb4c321fad2543b25a6ca6e0e80c16c465f8d25c
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-10-05 19:49:41 -0300

    Green AgregarEventoValido

--- src/Parque.Aplicacion/ServicioEvento.cs
+++ src/Parque.Aplicacion/ServicioEvento.cs
@@ -6,5 +6,10 @@ namespace Parque.Aplicacion;
 public class ServicioEvento(IRepositorio<Evento> repositorioEvento) : IServicioEvento
 {
     private readonly IRepositorio<Evento> _repositorioEvento = repositorioEvento;
+    public Evento AgregarEvento(Evento evento)
+    {
+        _repositorioEvento.Agregar(evento);
+        return evento;
+    }
 }
 
