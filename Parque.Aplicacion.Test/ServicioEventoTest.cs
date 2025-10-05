3e231c33309d022f66aabbe4d014edf922922a41
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-10-05 20:03:53 -0300

    Elimine pruebas Erradas

--- Parque.Aplicacion.Test/ServicioEventoTest.cs
+++ Parque.Aplicacion.Test/ServicioEventoTest.cs
@@ -8,25 +8,5 @@ namespace Parque.Aplicacion.Test;
 [TestClass]
 public class ServicioEventoTest
 {
-    [TestMethod]
-    public void AgregarEventoValido()
-    {
-        var mockRepositorio = new Mock<IRepositorio<Evento>>();
-        mockRepositorio.Setup(r => r.ObtenerTodos()).Returns(new List<Evento>());
-
-        var servicio = new ServicioEvento(mockRepositorio.Object);
-
-        var evento = new Evento(
-            "Noche de Dinosaurios",
-            "Temático",
-            new DateTime(2025, 12, 20, 20, 0, 0),
-            new DateTime(2025, 12, 20, 23, 0, 0),
-            300,
-            500f,
-            EstadoEvento.Programado);
-        var resultado = servicio.AgregarEvento(evento);
-
-        mockRepositorio.Verify(r => r.Agregar(It.Is<Evento>(e => e == evento)), Times.Once);
-        Assert.Equals("Noche de Dinosaurios", resultado.Titulo);
-    }
+    private Mock<IRepositorio<Evento>> _repositorioMock;
 }
