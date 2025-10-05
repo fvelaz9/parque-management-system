e9b2a42882c64d5da2b8048aa84eb5909ef28133
Felipe Velazquez <feli.velazquez@gmail.com>
2025-10-05 14:35:49 -0300

    Update Program.cs

--- src/Parque.WebApi/Program.cs
+++ src/Parque.WebApi/Program.cs
@@ -7,25 +7,33 @@ using Parque.Infraestructura.Repositorios;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddScoped(typeof(IRepositorio<>), typeof(Repositorio<>));
 
 builder.Services.AddSingleton<IServicioFechaHora, ServicioFechaHora>();
 builder.Services.AddScoped<IServicioAtracciones, ServicioAtracciones>();
 builder.Services.AddScoped<IServicioTicket, ServicioTicket>();
-
-builder.Services.AddDbContext<AppContexto>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+builder.Services.AddDbContext<AppContexto>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlServerOptionsAction: sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null);
+        }));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppContexto>();
+    db.Database.EnsureCreated();
+}
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
