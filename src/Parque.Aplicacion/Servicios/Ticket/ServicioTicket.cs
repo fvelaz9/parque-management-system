7db25f0c18ca28f525c7cfc114ea040936cac0ac
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-10-06 20:31:44 -0300

    Green validar evento ticket especial

--- src/Parque.Aplicacion/Servicios/Ticket/ServicioTicket.cs
+++ src/Parque.Aplicacion/Servicios/Ticket/ServicioTicket.cs
@@ -20,6 +20,7 @@ public class ServicioTicket(IRepositorio<Dominio.Ticket> repositorio, IRepositor
     public Dominio.Ticket CrearTicketEventoEspecial(int cuentaId, DateTime fechaVisita, int eventoId)
     {
         ValidarFechaFutura(fechaVisita);
+        ValidarEventoParaTicketEspecial(TipoTicket.EventoEspecial, eventoId);
         Dominio.Ticket ticket = new Dominio.Ticket()
         {
             CuentaId = cuentaId,
