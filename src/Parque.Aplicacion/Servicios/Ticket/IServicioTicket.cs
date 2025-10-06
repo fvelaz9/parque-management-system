6d57d5ef64fe90af129d8a4acd63e2f196dc4a58
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-10-06 20:09:10 -0300

    Funcion TickerEspecial

--- src/Parque.Aplicacion/Servicios/Ticket/IServicioTicket.cs
+++ src/Parque.Aplicacion/Servicios/Ticket/IServicioTicket.cs
@@ -5,6 +5,7 @@ namespace Parque.Aplicacion.Servicios.Ticket;
 public interface IServicioTicket
 {
     Dominio.Ticket CrearTicketGeneral(int cuentaId, DateTime fechaVisita);
+    Dominio.Ticket CrearTicketEventoEspecial(int cuentaId, DateTime fechaVisita, int eventoId);
     public IEnumerable<Dominio.Ticket> ListarTickets();
     Dominio.Ticket? BuscarTicket(int id);
     Dominio.Ticket? BuscarTicketPorCodigo(Guid codigo);
