f14022818fc065d6b51580d9473cc3c647182db9
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-10-06 02:24:17 -0300

    Red Excepcion con Evento Invalido

--- Parque.Aplicacion.Test/ServicioEventoTest.cs
+++ Parque.Aplicacion.Test/ServicioEventoTest.cs
@@ -38,4 +38,20 @@ public class ServicioEventoTest
         Assert.AreEqual(evento.Titulo, resultado.Titulo);
         _mockRepositorio.Verify(r => r.Agregar(evento), Times.Once);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void AgregarEventoFechaFinMenorAInicio()
+    {
+        var evento = new Evento(
+            titulo: "Evento inválido",
+            descripcion: "El fin es antes del inicio",
+            inicio: DateTime.Today.AddDays(3).AddHours(20),
+            fin: DateTime.Today.AddDays(3).AddHours(18),
+            aforoMaximo: 300,
+            costoAdicional: 25f,
+            estado: EstadoEvento.Cancelado);
+
+        _servicioEvento!.AgregarEvento(evento);
+    }
 }
