b21d6bc4b90c5f21d9b5058bb5ccfba4bd2a1ddb
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-10-06 21:09:15 -0300

    test getall valido

--- tests/Parque.WebApi.Test/Controllers/TicketControllerTest.cs
+++ tests/Parque.WebApi.Test/Controllers/TicketControllerTest.cs
@@ -1,6 +1,41 @@
-namespace Parque.WebApi.Test.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Parque.Aplicacion.Servicios.Ticket;
+using Parque.Dominio;
+using Parque.WebApi.Controllers;
 
+namespace Parque.WebApi.Test.Controllers;
+[TestClass]
 public class TicketControllerTest
 {
-    
+    private Mock<IServicioTicket>? _servicioMock;
+    private TicketController? _controller;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _servicioMock = new Mock<IServicioTicket>(MockBehavior.Strict);
+        _controller = new TicketController(_servicioMock.Object);
+    }
+
+    [TestMethod]
+    public void GetAllValido()
+    {
+        List<Ticket> tickets = new List<Ticket>
+        {
+            new Ticket { Id = 1, CuentaId = 1 },
+            new Ticket { Id = 2, CuentaId = 2 }
+        };
+
+        _servicioMock!.Setup(s => s.ListarTickets()).Returns(tickets);
+
+        var result = _controller!.GetAll();
+
+        var okResult = result as OkObjectResult;
+        Assert.IsNotNull(okResult);
+        Assert.AreEqual(200, okResult.StatusCode);
+        var returnedTickets = okResult.Value as IEnumerable<Ticket>;
+        Assert.AreEqual(2, returnedTickets!.Count());
+        _servicioMock.VerifyAll();
+    }
 }
