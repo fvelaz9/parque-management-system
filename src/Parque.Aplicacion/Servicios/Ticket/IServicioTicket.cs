a71f635ce709c8142fe172b7806af5aa0cb08252
Felipe Velazquez <feli.velazquez@gmail.com>
2025-10-06 23:11:55 -0300

    terminacion gestion incidencias

--- src/Parque.Aplicacion/Servicios/Ticket/IServicioTicket.cs
+++ src/Parque.Aplicacion/Servicios/Ticket/IServicioTicket.cs
@@ -5,7 +5,7 @@ namespace Parque.Aplicacion.Servicios.Ticket;
 public interface IServicioTicket
 {
     Dominio.Ticket CrearTicket(int cuentaId, DateTime fechaVisita, int? eventoId, TipoTicket tipoTicket);
-    public IEnumerable<Dominio.Ticket> ListarTickets();
+    IEnumerable<Dominio.Ticket> ListarTickets();
     Dominio.Ticket? BuscarTicket(int id);
     Dominio.Ticket? BuscarTicketPorCodigo(Guid codigo);
     void ModificarTicket(int id, int cuentaId, DateTime fechaVisita, int? eventoId, TipoTicket tipoTicket);
---
+++ src/Parque.WebApi/Controllers/IncidenciasController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Parque.Aplicacion.DTOS;
+using Parque.Aplicacion.Servicios.Incidencias;
+
+namespace Parque.WebApi.Controllers;
+
+[ApiController]
+[Route("api/incidencias")]
+public class IncidenciasController : ControllerBase
+{
+    private readonly IServicioIncidencia _servicio;
+
+    public IncidenciasController(IServicioIncidencia servicio)
+    {
+        _servicio = servicio;
+    }
+
+    [HttpPost]
+    public IActionResult CrearIncidencia([FromBody] CrearIncidenciaRequest request)
+    {
+        try
+        {
+            var incidencia = _servicio.CrearIncidencia(request);
+            return Ok(incidencia);
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new { mensaje = ex.Message });
+        }
+    }
+
+    [HttpGet("atraccion/{atraccionId}/disponible")]
+    public IActionResult VerificarDisponibilidad(int atraccionId)
+    {
+        try
+        {
+            var disponible = _servicio.EstaDisponible(atraccionId);
+            return Ok(new { atraccionId, disponible });
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new { mensaje = ex.Message });  
+        }
+    }
+}
