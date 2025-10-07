545abed9e2dfe3c8aeb0f6fdfb3e1b9184566d87
Felipe Velazquez <feli.velazquez@gmail.com>
2025-10-07 11:43:50 -0300

    cambio int a guid en cuentaId ticket

--- src/Parque.Aplicacion/DTOS/UpdateTicketDto.cs
+++ src/Parque.Aplicacion/DTOS/UpdateTicketDto.cs
@@ -4,7 +4,7 @@ namespace Parque.Aplicacion.DTOS;
 
 public class UpdateTicketDto
 {
-    public int CuentaId { get; set; }
+    public Guid CuentaId { get; set; }
     public DateTime FechaVisita { get; set; }
     public int? EventoId { get; set; }
     public TipoTicket TipoEntrada { get; set; }
