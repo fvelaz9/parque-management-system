31e21defe106f76283ec9966333199f32f1dac1b
Felipe Velazquez <feli.velazquez@gmail.com>
2025-10-08 10:02:42 -0300

    DbContext y program.cs modificados para puntaje

--- src/Parque.WebApi/Program.cs
+++ src/Parque.WebApi/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Parque.Aplicacion.Servicios;
 using Parque.Aplicacion.Servicios.Atracciones;
+using Parque.Aplicacion.Servicios.Gamificacion;
 using Parque.Aplicacion.Servicios.Ticket;
+using Parque.Dominio.Gamificacion;
 using Parque.Infraestructura;
 using Parque.Infraestructura.Repositorios;
 
@@ -20,6 +22,22 @@ builder.Services.AddScoped<IServicioAtracciones, ServicioAtracciones>();
 builder.Services.AddScoped<IServicioTicket, ServicioTicket>();
 
 builder.Services.AddScoped<IServicioEvento, ServicioEvento>();
+
+builder.Services.AddScoped<IEstrategiaPuntuacion, PuntuacionPorAtraccion>(sp =>
+    new PuntuacionPorAtraccion());
+
+builder.Services.AddScoped<IEstrategiaPuntuacion, PuntuacionCombo>(sp =>
+    new PuntuacionCombo(
+        minutosVentana: 10,
+        atraccionesMinimasCombo: 3,
+        puntosBase: 8,
+        puntosCombo: 25
+    ));
+
+builder.Services.AddScoped<IEstrategiaPuntuacion, PuntuacionPorEvento>(sp =>
+    new PuntuacionPorEvento());
+
+builder.Services.AddScoped<IServicioPuntuacion, ServicioPuntuacion>();
 
 builder.Services.AddDbContext<AppContexto>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 
