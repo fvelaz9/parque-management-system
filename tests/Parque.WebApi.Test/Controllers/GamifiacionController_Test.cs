76fc30138fc8a3c0dae9988e96015c7d90d70a98
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-10-08 16:00:03 -0300

    using eliminado

--- tests/Parque.WebApi.Test/Controllers/GamifiacionController_Test.cs
+++ tests/Parque.WebApi.Test/Controllers/GamifiacionController_Test.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Parque.Aplicacion.DTOS.Gamificacion;
 using Parque.Aplicacion.Servicios.Gamificacion;
