497b4c22a5b2286bc9d6f95e395ab24285324d59
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-10-08 16:00:22 -0300

    elimino using

--- tests/Parque.Aplicacion.Test/Servicios/ServicioPuntuacionTest.cs
+++ tests/Parque.Aplicacion.Test/Servicios/ServicioPuntuacionTest.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using Parque.Aplicacion.DTOS.Gamificacion;
 using Parque.Aplicacion.Servicios.Gamificacion;
 using Parque.Dominio;
 using Parque.Dominio.Atracciones;
