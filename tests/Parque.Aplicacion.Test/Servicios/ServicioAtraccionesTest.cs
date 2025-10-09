12a2dfd61595aa8cd56713fafdc399430b70cfd7
Felipe Velazquez <feli.velazquez@gmail.com>
2025-10-08 21:32:48 -0300

    cobertura 100% servicvioatraccion

--- tests/Parque.Aplicacion.Test/Servicios/ServicioAtraccionesTest.cs
+++ tests/Parque.Aplicacion.Test/Servicios/ServicioAtraccionesTest.cs
@@ -140,4 +140,55 @@ public class ServicioAtraccionesTest
         Assert.AreEqual(0, resultado.AforoActual);
         Assert.AreEqual(12, resultado.Disponible);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ObtenerAforoActual_AtraccionNoExiste_LanzaExcepcion()
+    {
+        _mockRepo.Setup(r => r.Encontrar(It.IsAny<Expression<Func<AtraccionParque, bool>>>()))
+            .Returns((AtraccionParque?)null);
+
+        _servicio.ObtenerAforoActual(99);
+    }
+
+    [TestMethod]
+    public void ObtenerReporteUso_ConRegistros_RetornaReporteOrdenado()
+    {
+        var fechaInicio = new DateTime(2025, 1, 1);
+        var fechaFin = new DateTime(2025, 12, 31);
+
+        var atraccion1 = new AtraccionParque("Montaña Rusa", TipoAtraccion.MontañaRusa, 12, 24, "Test") { Id = 1 };
+        var atraccion2 = new AtraccionParque("Carrusel", TipoAtraccion.Simulador, 0, 30, "Test") { Id = 2 };
+
+        var registros = new List<RegistroVisita>
+        {
+            new() { AtraccionId = 1, FechaIngreso = new DateTime(2025, 6, 1), FechaEgreso = new DateTime(2025, 6, 1, 1, 0, 0) },
+            new() { AtraccionId = 1, FechaIngreso = new DateTime(2025, 6, 2), FechaEgreso = new DateTime(2025, 6, 2, 1, 0, 0) },
+            new() { AtraccionId = 3, FechaIngreso = new DateTime(2025, 6, 3), FechaEgreso = new DateTime(2025, 6, 3, 1, 0, 0) }
+        };
+
+        _mockRepoRegistros.Setup(r => r.ObtenerTodos()).Returns(registros);
+        _mockRepo.Setup(r => r.Encontrar(It.Is<Expression<Func<AtraccionParque, bool>>>(expr => true)))
+            .Returns<Expression<Func<AtraccionParque, bool>>>(expr =>
+            {
+                var func = expr.Compile();
+                if(func(atraccion1))
+                {
+                    return atraccion1;
+                }
+
+                if(func(atraccion2))
+                {
+                    return atraccion2;
+                }
+
+                return null;
+            });
+
+        var resultado = _servicio.ObtenerReporteUso(fechaInicio, fechaFin);
+
+        Assert.AreEqual(2, resultado.Count);
+        Assert.AreEqual("Montaña Rusa", resultado[0].NombreAtraccion);
+        Assert.AreEqual("Desconocida", resultado[1].NombreAtraccion);
+    }
 }
