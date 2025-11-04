381ad564fec1702cb2afd8c4e6d0b74b8340a8fe
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-11-04 18:39:42 -0300

    atributos y constructor

--- src/Parque.Dominio/HistorialPuntuacion.cs
+++ src/Parque.Dominio/HistorialPuntuacion.cs
@@ -2,5 +2,21 @@ namespace Parque.Dominio;
 
 public class HistorialPuntuacion
 {
-    
+    public int Id { get; set; }
+    public DateTime FechaHora { get; set; }
+    public string OrigenPuntos { get; set; } = string.Empty;
+    public string EstrategiaActiva { get; set; } = string.Empty;
+    public int Puntos { get; set; }
+
+    public HistorialPuntuacion()
+    {
+    }
+
+    public HistorialPuntuacion(DateTime fechaHora, string origenPuntos, string estrategiaActiva, int puntos)
+    {
+        FechaHora = fechaHora;
+        OrigenPuntos = origenPuntos;
+        EstrategiaActiva = estrategiaActiva;
+        Puntos = puntos;
+    }
 }
