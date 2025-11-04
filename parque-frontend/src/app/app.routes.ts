cf8ffd99cdfc9c1936751d1d1e14978d963a05a5
tomas pons <t1999pons@gmail.com>
2025-11-04 17:24:58 -0300

    feat: se agrega guard y componente not found

--- parque-frontend/src/app/app.routes.ts
+++ parque-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home';
+import { NotFoundComponent } from './features/not-found/not-found';
 import { AtraccionListComponent } from './features/atracciones/atraccion-list/atraccion-list';
+import { authGuard } from './auth-guard';
 
 export const routes: Routes = [
   {
@@ -15,12 +17,17 @@ export const routes: Routes = [
   {
     path: 'atracciones',
     loadComponent: () => import('./features/atracciones/atraccion-list/atraccion-list')
-      .then(m => m.AtraccionListComponent)
+      .then(m => m.AtraccionListComponent),
+      canActivate: [authGuard]
   },
   {
     path: 'login',
     loadComponent: () => import('./features/auth/login/login')
       .then(m => m.LoginComponent)
   },
-
+  // Not Found route - debe ser la última
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
 ];
