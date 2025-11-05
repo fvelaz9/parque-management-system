78af3f129c60900b3b8d09d2ca5e9aa12bad676e
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-11-05 17:29:47 -0300

    DTO HISTORIAL

--- src/Parque.Aplicacion/DTOS/Gamificacion/HistorialPuntuacionDto.cs
+++ src/Parque.Aplicacion/DTOS/Gamificacion/HistorialPuntuacionDto.cs
@@ -2,5 +2,8 @@ namespace Parque.Aplicacion.DTOs.Gamificacion;
 
 public class HistorialPuntuacionDto
 {
-    
+    public DateTime FechaHora { get; set; }
+    public string OrigenPuntos { get; set; } = string.Empty;
+    public string EstrategiaActiva { get; set; } = string.Empty;
+    public int Puntos { get; set; }
 }
