292f1ea0d211467b16a469c0d1192afd6f397ec5
tomas pons <t1999pons@gmail.com>
2025-11-07 18:07:47 -0300

    nuevas migraciones

---
+++ src/Parque.Infraestructura/Migrations/20251107204044_InitialCreate.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parque.Infraestructura.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Atracciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    EdadMinima = table.Column<int>(type: "int", nullable: false),
+                    Capacidad = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Atracciones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConfiguracionesEstrategia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EstrategiaActiva = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FechaModificacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfiguracionesEstrategia", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConfiguracionFechaHora",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaHoraConfigurada = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfiguracionFechaHora", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Eventos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Inicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Fin = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AforoMaximo = table.Column<int>(type: "int", nullable: false),
+                    CostoAdicional = table.Column<float>(type: "real", nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Eventos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Incidencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaReporte = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaResolucionEstimada = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Disponible = table.Column<bool>(type: "bit", nullable: false),
+                    AtraccionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incidencias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PuntuacionesVisitantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VisitanteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PuntosDiarios = table.Column<int>(type: "int", nullable: false),
+                    PuntosTotales = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PuntuacionesVisitantes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegistrosVisitas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AtraccionId = table.Column<int>(type: "int", nullable: false),
+                    Identificador = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FechaIngreso = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaEgreso = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistrosVisitas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sesiones",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sesiones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CuentaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FechaVisita = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventoId = table.Column<int>(type: "int", nullable: true),
+                    TipoEntrada = table.Column<int>(type: "int", nullable: false),
+                    Codigo = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FechaEmision = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EsValido = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Visitantes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NivelMembresia = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visitantes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventoAtracciones",
+                columns: table => new
+                {
+                    AtraccionesId = table.Column<int>(type: "int", nullable: false),
+                    EventoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventoAtracciones", x => new { x.AtraccionesId, x.EventoId });
+                    table.ForeignKey(
+                        name: "FK_EventoAtracciones_Atracciones_AtraccionesId",
+                        column: x => x.AtraccionesId,
+                        principalTable: "Atracciones",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventoAtracciones_Eventos_EventoId",
+                        column: x => x.EventoId,
+                        principalTable: "Eventos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MantenimientosPreventivos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AtraccionId = table.Column<int>(type: "int", nullable: false),
+                    FechaProgramada = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HoraInicio = table.Column<TimeSpan>(type: "time", nullable: false),
+                    DuracionEstimada = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IncidenciaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MantenimientosPreventivos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MantenimientosPreventivos_Incidencias_IncidenciaId",
+                        column: x => x.IncidenciaId,
+                        principalTable: "Incidencias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cuentas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    VisitanteId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Roles = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuentas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cuentas_Visitantes_VisitanteId",
+                        column: x => x.VisitanteId,
+                        principalTable: "Visitantes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cuentas_VisitanteId",
+                table: "Cuentas",
+                column: "VisitanteId",
+                unique: true,
+                filter: "[VisitanteId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventoAtracciones_EventoId",
+                table: "EventoAtracciones",
+                column: "EventoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MantenimientosPreventivos_IncidenciaId",
+                table: "MantenimientosPreventivos",
+                column: "IncidenciaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PuntuacionesVisitantes_VisitanteId_Fecha",
+                table: "PuntuacionesVisitantes",
+                columns: new[] { "VisitanteId", "Fecha" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sesiones_Token",
+                table: "Sesiones",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConfiguracionesEstrategia");
+
+            migrationBuilder.DropTable(
+                name: "ConfiguracionFechaHora");
+
+            migrationBuilder.DropTable(
+                name: "Cuentas");
+
+            migrationBuilder.DropTable(
+                name: "EventoAtracciones");
+
+            migrationBuilder.DropTable(
+                name: "MantenimientosPreventivos");
+
+            migrationBuilder.DropTable(
+                name: "PuntuacionesVisitantes");
+
+            migrationBuilder.DropTable(
+                name: "RegistrosVisitas");
+
+            migrationBuilder.DropTable(
+                name: "Sesiones");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Visitantes");
+
+            migrationBuilder.DropTable(
+                name: "Atracciones");
+
+            migrationBuilder.DropTable(
+                name: "Eventos");
+
+            migrationBuilder.DropTable(
+                name: "Incidencias");
+        }
+    }
+}
