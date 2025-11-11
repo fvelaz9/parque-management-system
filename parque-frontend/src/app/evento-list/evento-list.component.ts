de9a140235f852b974aa779b3cb5caaaa876f367
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-11-10 22:58:39 -0300

    vemtana evento-list

---
+++ parque-frontend/src/app/evento-list/evento-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evento-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './evento-list.component.html',
+  styleUrl: './evento-list.component.css'
+})
+export class EventoListComponent {
+
+}
