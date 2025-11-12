9d4ca8ef556e36f61c726052588014f6294ee038
Felipe Velazquez <feli.velazquez@gmail.com>
2025-11-11 23:41:59 -0300

    Update ServicioRecompensaTest.cs

--- tests/Parque.Aplicacion.Test/Servicios/ServicioRecompensaTest.cs
+++ tests/Parque.Aplicacion.Test/Servicios/ServicioRecompensaTest.cs
@@ -1,4 +1,5 @@
 using Moq;
+using Parque.Aplicacion.DTOs.RecompensasDtos;
 using Parque.Aplicacion.Servicios.Recompensas;
 using Parque.Dominio.Gamificacion;
 using Parque.Dominio.Usuarios;
@@ -9,25 +10,4 @@ namespace Parque.Aplicacion.Test.Servicios;
 [TestClass]
 public class ServicioRecompensaTest
 {
-    private Mock<IRepositorio<Recompensa>> _mockRepoRecompensa = null!;
-    private Mock<IRepositorio<HistorialCanje>> _mockRepoHistorial = null!;
-    private Mock<IRepositorio<PuntuacionVisitante>> _mockRepoPuntuacion = null!;
-    private Mock<IRepositorio<Visitante>> _mockRepoVisitante = null!;
-    private ServicioRecompensa _servicio = null!;
-
-    [TestInitialize]
-    public void Setup()
-    {
-        _mockRepoRecompensa = new Mock<IRepositorio<Recompensa>>();
-        _mockRepoHistorial = new Mock<IRepositorio<HistorialCanje>>();
-        _mockRepoPuntuacion = new Mock<IRepositorio<PuntuacionVisitante>>();
-        _mockRepoVisitante = new Mock<IRepositorio<Visitante>>();
-        
-        _servicio = new ServicioRecompensa(
-            _mockRepoRecompensa.Object,
-            _mockRepoHistorial.Object,
-            _mockRepoPuntuacion.Object,
-            _mockRepoVisitante.Object
-        );
-    }
 }
