acb9d349c8dc78d07aaa7752fb0df6c546874046
tomas pons <t1999pons@gmail.com>
2025-11-13 21:46:25 -0300

    feat: se agregan headers con auth en peticiones de recompensa

--- parque-frontend/src/app/core/services/recompensa.service.ts
+++ parque-frontend/src/app/core/services/recompensa.service.ts
@@ -10,44 +10,44 @@ import { AuthService } from './auth.service';
 @Injectable({ providedIn: 'root' })
 export class RecompensasService {
   private readonly http = inject(HttpClient);
-  private authService = inject(AuthService);
   private readonly apiUrl = `${environment.apiUrl}/recompensas`;
+  private readonly authService = inject(AuthService);
 
   private getHeaders(): HttpHeaders {
-    const token = localStorage.getItem('token');
+    const token = this.authService.getToken();
     return new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': token || ''  // ← SIN "Bearer"
+      'Authorization': `${token}`
     });
   }
 
   // GET /api/recompensas
   getAll(): Observable<{ total: number; recompensas: Recompensa[] }> {
-    return this.http.get<{ total: number; recompensas: Recompensa[] }>(this.apiUrl);
+    return this.http.get<{ total: number; recompensas: Recompensa[] }>(this.apiUrl, { headers: this.getHeaders() });
   }
 
   // GET /api/recompensas/{id}
   getById(id: string): Observable<Recompensa> {
-    return this.http.get<Recompensa>(`${this.apiUrl}/${id}`, {headers: this.getHeaders()});
+    return this.http.get<Recompensa>(`${this.apiUrl}/${id}`, {headers: this.getHeaders() });
   }
 
   // POST /api/recompensas
   create(request: CrearRecompensaRequest): Observable<any> {
-    return this.http.post(this.apiUrl, request);
+    return this.http.post(this.apiUrl, request, { headers: this.getHeaders() });
   }
 
   // PUT /api/recompensas/{id}
   update(id: string, request: CrearRecompensaRequest): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, request);
+    return this.http.put(`${this.apiUrl}/${id}`, request, { headers: this.getHeaders() });
   }
 
   // POST /api/recompensas/canjear
   canjear(request: CanjearRecompensaRequest): Observable<any> {
-    return this.http.post(`${this.apiUrl}/canjear`, request);
+    return this.http.post(`${this.apiUrl}/canjear`, request, { headers: this.getHeaders() });
   }
 
   // GET /api/recompensas/historial/{visitanteId}
   getHistorial(visitanteId: string): Observable<{ visitanteId: string; totalCanjes: number; historial: HistorialCanjeDto[] }> {
-    return this.http.get<{ visitanteId: string; totalCanjes: number; historial: HistorialCanjeDto[] }>(`${this.apiUrl}/historial/${visitanteId}`);
+    return this.http.get<{ visitanteId: string; totalCanjes: number; historial: HistorialCanjeDto[] }>(`${this.apiUrl}/historial/${visitanteId}`, { headers: this.getHeaders() });
   }
 }
