aca2a428907f3f66dea651a330e011b82200d140
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-11-16 17:58:01 -0300

    componente acceso home

---
+++ parque-frontend/src/app/features/Acceso/acceso-home/acceso-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceso-home',
+  imports: [],
+  templateUrl: './acceso-home.html',
+  styleUrl: './acceso-home.css',
+})
+export class AccesoHome {
+
+}
