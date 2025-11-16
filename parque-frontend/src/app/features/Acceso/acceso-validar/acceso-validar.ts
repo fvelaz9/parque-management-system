db129423b06132eb246f5a338d3ddb8c5e9fdef3
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-11-16 18:17:12 -0300

    componente validar acceso

---
+++ parque-frontend/src/app/features/Acceso/acceso-validar/acceso-validar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceso-validar',
+  imports: [],
+  templateUrl: './acceso-validar.html',
+  styleUrl: './acceso-validar.css',
+})
+export class AccesoValidar {
+
+}
