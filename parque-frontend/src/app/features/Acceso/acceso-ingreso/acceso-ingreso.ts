6635ce9caa4e50a913c7166abad9c324141a8a1f
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-11-16 19:29:49 -0300

    componente acceso-ingreso

---
+++ parque-frontend/src/app/features/Acceso/acceso-ingreso/acceso-ingreso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceso-ingreso',
+  imports: [],
+  templateUrl: './acceso-ingreso.html',
+  styleUrl: './acceso-ingreso.css',
+})
+export class AccesoIngreso {
+
+}
