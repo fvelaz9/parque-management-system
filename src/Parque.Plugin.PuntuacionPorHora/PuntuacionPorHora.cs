9bee9b516f35eef5aa3a55ddb6c2e7da7df7a6db
tomas pons <t1999pons@gmail.com>
2025-11-16 16:54:26 -0300

    nueva estrategia con puntuacion por hora

---
+++ src/Parque.Plugin.PuntuacionPorHora/PuntuacionPorHora.cs
@@ -0,0 +1,50 @@
+﻿using Parque.Dominio;
+using Parque.Dominio.Atracciones;
+using Parque.Dominio.Gamificacion;
+
+namespace Parque.Plugin.PuntuacionPorHora;
+
+public class PuntuacionPorHora : IEstrategiaPuntuacion
+{
+    private const int PUNTOS_BASE = 10;
+    private const int MULTIPLICADOR_PICO = 2;
+    private const double MULTIPLICADOR_VALLE = 0.5;
+
+    public string Nombre => "PorHora";
+
+    int IEstrategiaPuntuacion.CalcularPuntos(
+        RegistroVisita registro,
+        AtraccionParque atraccionParque,
+        List<RegistroVisita> historialDiario,
+        Evento? eventoActivo)
+    {
+        var hora = registro.FechaIngreso.Hour;
+
+        if(EsHorarioPico(hora))
+        {
+            return PUNTOS_BASE * MULTIPLICADOR_PICO;
+        }
+
+        if(EsHorarioValle(hora))
+        {
+            return (int)(PUNTOS_BASE * MULTIPLICADOR_VALLE);
+        }
+
+        return PUNTOS_BASE;
+    }
+
+    private static bool EsHorarioPico(int hora)
+    {
+        return (hora >= 10 && hora < 12) || (hora >= 18 && hora < 20);
+    }
+
+    private static bool EsHorarioValle(int hora)
+    {
+        return hora < 10 || hora >= 20;
+    }
+
+    public int CalcularPuntos(RegistroVisita registro, AtraccionParque atraccionParque, List<RegistroVisita> historialDiario, Evento? eventoActivo)
+    {
+        throw new NotImplementedException();
+    }
+}
