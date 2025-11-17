a35dfae6b89016182bb83a1e9a6d70c290df7ab4
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-11-17 18:23:53 -0300

    componente egreso

---
+++ parque-frontend/src/app/features/Acceso/acceso-egreso/acceso-egreso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceso-egreso',
+  imports: [],
+  templateUrl: './acceso-egreso.html',
+  styleUrl: './acceso-egreso.css',
+})
+export class AccesoEgreso {
+
+}
