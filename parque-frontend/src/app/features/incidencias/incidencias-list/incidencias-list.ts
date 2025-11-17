0f7a8b5b53371a1d68a295e44e53edac7c3237cb
Felipe Velazquez <feli.velazquez@gmail.com>
2025-11-16 22:58:08 -0300

    creacion interfaz incidencais

---
+++ parque-frontend/src/app/features/incidencias/incidencias-list/incidencias-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incidencias-list',
+  imports: [],
+  templateUrl: './incidencias-list.html',
+  styleUrl: './incidencias-list.css',
+})
+export class IncidenciasList {
+
+}
