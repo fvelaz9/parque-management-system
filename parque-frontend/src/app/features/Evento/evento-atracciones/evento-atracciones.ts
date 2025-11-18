f8a3467b7bae3a1e478e75acf0e5af3c244ee55c
Nicolas Cardozo <nicolacho.cardozo28@gmail.com>
2025-11-18 13:35:45 -0300

    componente para ver atracciones de un evento

---
+++ parque-frontend/src/app/features/Evento/evento-atracciones/evento-atracciones.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evento-atracciones',
+  imports: [],
+  templateUrl: './evento-atracciones.html',
+  styleUrl: './evento-atracciones.css',
+})
+export class EventoAtracciones {
+
+}
