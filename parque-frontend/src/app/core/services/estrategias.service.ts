8984acdba1739903c36fe63b4fc31b4498d288f1
tomas pons <t1999pons@gmail.com>
2025-11-18 17:05:34 -0300

    feat: se agrega interceptor para evitar repetir codigo de header (DRY)

--- parque-frontend/src/app/core/services/estrategias.service.ts
+++ parque-frontend/src/app/core/services/estrategias.service.ts
@@ -1,66 +1,33 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
 import { environment } from '../../../environments/environment.development';
-// import { ResponseDto, EstrategiaInfo, CambiarEstrategiaRequest } from '../models/estrategia.model';
-
-export interface EstrategiaInfo {
-  nombre: string;
-  descripcion: string;
-  origen: 'Base' | 'Plugin';
-  esActiva: boolean;
-  parametros?: string[];
-}
-
-export interface ResponseDto<T = any> {
-  content: T;
-  executionSuccessful: boolean;
-  message: string;
-}
-
-export interface CambiarEstrategiaRequest {
-  nombreEstrategia: string;
-}
+import { ResponseDto, EstrategiaInfo, CambiarEstrategiaRequest } from '../models/estrategia.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstrategiasService {
   private http = inject(HttpClient);
-  private authService = inject(AuthService);
   private apiUrl = `${environment.apiUrl}/estrategias`;
 
-  private getHeaders(): HttpHeaders {
-    const token = this.authService.getToken();
-    return new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `${token}`
-    });
-  }
-
   obtenerEstrategiasDisponibles(): Observable<ResponseDto<EstrategiaInfo[]>> {
-    return this.http.get<ResponseDto<EstrategiaInfo[]>>(`${this.apiUrl}/disponibles`, { headers: this.getHeaders() });
+    return this.http.get<ResponseDto<EstrategiaInfo[]>>(`${this.apiUrl}/disponibles`);
   }
 
   obtenerEstrategiaActiva(): Observable<ResponseDto<{ estrategiaActiva: string }>> {
-    return this.http.get<ResponseDto<{ estrategiaActiva: string }>>(`${this.apiUrl}/activa`, { headers: this.getHeaders() });
+    return this.http.get<ResponseDto<{ estrategiaActiva: string }>>(`${this.apiUrl}/activa`);
   }
 
   cambiarEstrategiaActiva(nombreEstrategia: string): Observable<ResponseDto<{ nuevaEstrategia: string }>> {
     const request: CambiarEstrategiaRequest = { nombreEstrategia };
     return this.http.put<ResponseDto<{ nuevaEstrategia: string }>>(
-      `${this.apiUrl}/activa`,
-      request,
-      { headers: this.getHeaders() }
-    );
+      `${this.apiUrl}/activa`, request);
   }
 
   recargarPlugins(): Observable<ResponseDto<EstrategiaInfo[]>> {
     return this.http.post<ResponseDto<EstrategiaInfo[]>>(
       `${this.apiUrl}/plugins/recargar`,
-      {},
-      { headers: this.getHeaders() }
-    );
+      {});
   }
 }
