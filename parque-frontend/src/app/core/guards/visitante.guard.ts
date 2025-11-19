8d0d751b80c3501e2c9b226da83bcb050847022e
Felipe Velazquez <feli.velazquez@gmail.com>
2025-11-19 00:42:24 -0300

    creacion historial puntaje

--- parque-frontend/src/app/core/guards/visitante.guard.ts
+++ parque-frontend/src/app/core/guards/visitante.guard.ts
@@ -6,13 +6,11 @@ export const visitanteGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  // Primero verificar si está autenticado
   if (!authService.isLoggedIn()) {
     router.navigate(['/login']);
     return false;
   }
 
-  // Luego verificar si tiene rol de visitante
   if (!authService.tieneRol('Visitante')) {
     router.navigate(['/home']);
     return false;
---
+++ parque-frontend/src/app/features/historial-puntuacion/historial-puntuacion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-puntuacion',
+  imports: [],
+  templateUrl: './historial-puntuacion.html',
+  styleUrl: './historial-puntuacion.css',
+})
+export class HistorialPuntuacion {
+
+}
